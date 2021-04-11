073eb280e927ed4ae2b8e0edaa72830e6ab8ad2f
Gabriel-Guariglia <gabrielguariglia@gmail.com>
2021-04-11 15:46:45 -0300

    mudando chave

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   async function obtemMoeda(moeda) {
     setObtendoMoeda(true)
     let chaveAPI = process.env.REACT_APP_APIKEY
-    let url = `https://thingproxy.freeboard.io/fetch/https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,${moeda}&key=${chaveAPI}`
+    let url = `https://api.hgbrasil.com/finance?array_limit=1&fields=only_results,${moeda}&key=${chaveAPI}`
     await fetch(url)
     
       .then(response => response.json())
